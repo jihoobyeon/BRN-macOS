f9b4cfa1f74dc3bb8ccbc25c163c3426b3bb24b2
Jihoo Byeon <jihoobyeon@gmail.com>
2023-07-03 19:01:33 +0900

    Update EngineHook.d.ts

--- EngineHook.d.ts
+++ EngineHook.d.ts
@@ -1,2 +1,25 @@
 import { Engine } from '@babylonjs/core';
-export declare function useEngine(): Engine | undefined;
+import { Nullable } from '@babylonjs/core/types';
+import { Engine } from '@babylonjs/core/Engines/engine';
+import { ReactNativeEngine } from '@babylonjs/react-native';
+
+declare function useEngine(): Engine | undefined;
+type EngineProps = { engine?: Engine };
+
+declare type WithEngineProps = {
+	abortController: Nullable<AbortController>,
+	engine: Nullable<Engine>
+};
+
+declare function withEngine<P extends WithEngineProps>(
+	Component: React.ComponentType<P>
+): React.ComponentType<Omit<P, keyof WithEngineProps>>;
+
+declare class EngineWrapper extends React.Component<WithEngineProps> {
+	constructor(props: WithEngineProps);
+	async componentDidMount(): Promise<void>;
+	componentWillUnmount(): void;
+	render(): JSX.Element;
+}
+
+export { EngineWrapper, useEngine, withEngine, WithEngineProps };
